17aa5d3d4d61822007a2686cd8e258c2ff2d21f9
marliseu <marlise.au@gmail.com>
2016-04-16 19:23:32 -0400

    removed footer from register

--- views/register-page.php
+++ views/register-page.php
@@ -3,12 +3,6 @@
       <div class="container">
         <br><br>
         <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
-        <div class="row center">
-          <h5 class="header col s12 light">Register</h5>
-        </div>
-        <div class="row center">
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
-        </div>
         <br><br>
 
       </div>
