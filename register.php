64029a0c1b859e67fecff1d8b914555d1c6cf898
Arturo J. Real <arturojreal@gmail.com>
2016-04-16 18:55:42 -0400

    more changes

--- register.php
+++ register.php
@@ -121,7 +121,7 @@
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      Made by <a class="brown-text text-lighten-3" href="http://cs50xmiami.com">Team50</a>
       </div>
     </div>
   </footer>
