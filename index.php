5bbb8786d5473d9a0eea442dd9f10cd7bb13e407
marliseu <marlise.au@gmail.com>
2016-04-16 19:28:37 -0400

    Renamed index tamplate

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require('views/head.php');
   require('views/navigation.php');
-  require('views/index-content.php');
+  require('views/index-page.php');
   require('views/footer.php');
   require('views/scripts.php');
 ?>
