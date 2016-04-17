7a205b7c76507e09477bf83bdbb20d785969f644
daniel chang <user@w341570.local>
2016-04-17 07:55:59 -0400

    the json thing

--- dashboard.php
+++ dashboard.php
@@ -2,9 +2,24 @@
 
   $title = "Dashboard";
 
-  require('visaDirect.php');
+
   require('views/head.php');
   require('views/navigation.php');
+
   require('views/dashboard-page.php');
+  $data = array("transactionIdentifier"=> 23423432,
+                "actionCode"=> "5",
+                "responseCode"=> "5",
+                "transmissionDateTime"=> "2016-04-17T11:30:10.000Z",
+                "cavvResultCode"=> "4",
+                "feeProgramIndicator"=> "123"
+              );
+  //header('Content-type: text/javascript');
+  echo "<h4> History </h4>\n";
+  echo "Transaction ID:  " . $data['transactionIdentifier'] . "  " .
+       "Action Code:  " . $data['actionCode'] . "  " .
+       "Date:  " . $data['transmissionDateTime'];
+  //$json_string = json_encode($data, JSON_PRETTY_PRINT);
+
   require('views/footer.php');
   require('views/scripts.php');
