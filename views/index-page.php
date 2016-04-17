836002bfca2d8658828f433beaf66d2503a3dab8
marliseu <marlise.au@gmail.com>
2016-04-16 23:59:10 -0400

    Un-fucked register

--- views/index-page.php
+++ views/index-page.php
@@ -4,27 +4,20 @@
       <br><br>
       <h1 class="header center white-text text-lighten-2">FIRST STEP</h1>
       <div class="row center">
-
         <h5 class="header col s12 light">Addiction can take over your life. <br />Free yourself.</h5>
-
       </div>
       <div class="row center col-sm-4">
             <a href="register.php" id="download-button" class="btn-large waves-effect waves-light brown">Get Started</a>
             <a href="login.php" id="download-button" class="btn-large waves-effect waves-light brown">Log In</a>
       </div>
       <br><br>
-
     </div>
   </div>
   <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
 </div>
-
-
 <div class="container">
   <h3 class="center black-text">HOW IT WORKS</h3>
-
   <div class="section">
-
     <!--   Icon Section   -->
     <div class="row">
       <div class="col s12 m4">
@@ -35,30 +28,23 @@
           <p class="light">Register any eligible VISA card with our application</p>
         </div>
       </div>
-
       <div class="col s12 m4">
         <div class="icon-block">
           <h2 class="center brown-text"><i class="material-icons">lock</i></h2>
           <h5 class="center">Set Restrictions</h5>
-
           <p class="light">Our Card Block feature enables card restrictions that block the purchase of alcohol or tobacco products from registered VISA cards</p>
         </div>
       </div>
-
       <div class="col s12 m4">
         <div class="icon-block">
           <h2 class="center brown-text"><i class="material-icons">error</i></h2>
           <h5 class="center">Receive Notifications</h5>
-
           <p class="light">Receive instant notifications for attempted purchases made with registered and restricted VISA cards</p>
         </div>
       </div>
     </div>
-
   </div>
 </div>
-
-
 <div class="parallax-container valign-wrapper">
   <div class="section no-pad-bot">
     <div class="container">
@@ -69,10 +55,8 @@
   </div>
   <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
 </div>
-
 <div class="container">
   <div class="section">
-
     <div class="row">
       <div class="col s12 m4">
         <div class="icon-block">
@@ -81,16 +65,13 @@
           <p class="light">According to the CDC, 20 percent of high school students smoke and 28 percent drinl. Research shows many teen smokers think cigarettes are cool. Some drink due to increased stress or family problems, while others drink out of curiosity or to fit in.</p>
         </div>
       </div>
-
       <div class="col s12 m4">
         <div class="icon-block">
           <h2 class="center brown-text"><i class="material-icons">local_drink</i></h2>
           <h5 class="center">Alcohol Abuse</h5>
-
           <p class="light">In 2014, 87.65% of people ages 18 or older reported that they drank alcohol at some point in their lifetime; 71.0 percent reported that they drank in the past year; 56.9 percent reported that they drank in the past month.</p>
         </div>
       </div>
-
       <div class="col s12 m4">
         <div class="icon-block">
           <h2 class="center brown-text"><i class="material-icons">smoking_rooms</i></h2>
@@ -99,20 +80,13 @@
         </div>
       </div>
     </div>
-
-
   </div>
 </div>
-
-
 <div class="parallax-container valign-wrapper">
   <div class="section no-pad-bot">
     <div class="container">
       <div class="row center">
-
-
         <h5 class="header col s12 light">Because the best way to avoid a tragedy is to prevent it.</h5>
-
       </div>
     </div>
   </div>
--- views/register-page.php
+++ views/register-page.php
@@ -12,24 +12,11 @@
   <div class="container">
     <div class="section">
       <form class="col s12">
-        <div class="row">
-          <div class="input-field col s6">
-            <i class="material-icons brown-text prefix">account_circle</i>
-            <input id="first_name" type="text" name="fname" class="validate" required>
-            <label for="first_name">First Name</label>
-
-<!-- <div class="container">
-    <div class="section">
- -->
-
-  <div class="card brown lighten-5">
-    <div class="card-content">
-
       <div class="row">
         <form class="col s12">
           <div class="row">
             <div class="input-field col s6">
-              <i class="material-icons prefix">account_circle</i>
+              <i class="material-icons brown-text prefix">account_circle</i>
               <input id="first_name" type="text" name="fname" class="validate" required>
               <label for="first_name">First Name</label>
             </div>
@@ -37,42 +24,41 @@
               <input id="last_name" type="text" name="lname" class="validate" required>
               <label for="last_name">Last Name</label>
             </div>
-
-
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons brown-text prefix">vpn_key</i>
-            <input id="password" type="password" name="password" class="validate" required>
-            <label for="password">Password</label>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons brown-text prefix">vpn_key</i>
-            <input id="password" type="password" name="password2" class="validate" required>
-            <label for="password">Password Confirm</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons brown-text prefix">vpn_key</i>
+              <input id="password" type="password" name="password" class="validate" required>
+              <label for="password">Password</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons brown-text prefix">email</i>
-            <input id="email" type="email" name="email" class="validate" required>
-            <label for="email">Email</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons brown-text prefix">vpn_key</i>
+              <input id="password" type="password" name="password2" class="validate" required>
+              <label for="password">Password Confirm</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons brown-text prefix">credit_card</i>
-            <input id="email" type="email" name="email" class="validate" required>
-            <label for="email">Credit Card Number</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons brown-text prefix">email</i>
+              <input id="email" type="email" name="email" class="validate" required>
+              <label for="email">Email</label>
+            </div>
           </div>
-        </div>
-        <div class="center-align">
-          <button class="btn waves-effect brown waves-light" type="submit" name="action">Submit
-            <i class="material-icons right">send</i>
-          </button>
-        </div>
-      </form>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons brown-text prefix">credit_card</i>
+              <input id="email" type="email" name="email" class="validate" required>
+              <label for="email">Credit Card Number</label>
+            </div>
+          </div>
+          <div class="center-align">
+            <button class="btn waves-effect brown waves-light" type="submit" name="action">Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+</div>
