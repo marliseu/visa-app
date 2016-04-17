565e88bf003113082ea5147606d43e720f89b60e
marliseu <marlise.au@gmail.com>
2016-04-16 23:34:10 -0400

    Full screen home page

--- register.php
+++ register.php
@@ -4,5 +4,6 @@
   require('views/head.php');
   require('views/navigation.php');
   require('views/register-page.php');
+  require('views/footer.php');
   require('views/scripts.php');
 ?>
--- views/register-page.php
+++ views/register-page.php
@@ -1,4 +1,4 @@
-  <div id="index-banner" class="parallax-container">
+  <div id="banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
@@ -8,55 +8,53 @@
     </div>
     <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
   </div>
-  <div class="card brown lighten-5">
-    <div class="card-content">
-      <div class="row">
-        <form class="col s12">
-          <div class="row">
-            <div class="input-field col s6">
-              <i class="material-icons brown-text prefix">account_circle</i>
-              <input id="first_name" type="text" name="fname" class="validate" required>
-              <label for="first_name">First Name</label>
-            </div>
-            <div class="input-field col s6">
-              <input id="last_name" type="text" name="lname" class="validate" required>
-              <label for="last_name">Last Name</label>
-            </div>
+  <div class="container">
+    <div class="section">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <i class="material-icons brown-text prefix">account_circle</i>
+            <input id="first_name" type="text" name="fname" class="validate" required>
+            <label for="first_name">First Name</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <i class="material-icons brown-text prefix">vpn_key</i>
-              <input id="password" type="password" name="password" class="validate" required>
-              <label for="password">Password</label>
-            </div>
+          <div class="input-field col s6">
+            <input id="last_name" type="text" name="lname" class="validate" required>
+            <label for="last_name">Last Name</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <i class="material-icons brown-text prefix">vpn_key</i>
-              <input id="password" type="password" name="password2" class="validate" required>
-              <label for="password">Password Confirm</label>
-            </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons brown-text prefix">vpn_key</i>
+            <input id="password" type="password" name="password" class="validate" required>
+            <label for="password">Password</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <i class="material-icons brown-text prefix">email</i>
-              <input id="email" type="email" name="email" class="validate" required>
-              <label for="email">Email</label>
-            </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons brown-text prefix">vpn_key</i>
+            <input id="password" type="password" name="password2" class="validate" required>
+            <label for="password">Password Confirm</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <i class="material-icons brown-text prefix">credit_card</i>
-              <input id="email" type="email" name="email" class="validate" required>
-              <label for="email">Credit Card Number</label>
-            </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons brown-text prefix">email</i>
+            <input id="email" type="email" name="email" class="validate" required>
+            <label for="email">Email</label>
           </div>
-          <div class="center-align">
-            <button class="btn waves-effect brown waves-light" type="submit" name="action">Submit
-              <i class="material-icons right">send</i>
-            </button>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons brown-text prefix">credit_card</i>
+            <input id="email" type="email" name="email" class="validate" required>
+            <label for="email">Credit Card Number</label>
           </div>
-        </form>
-      </div>
+        </div>
+        <div class="center-align">
+          <button class="btn waves-effect brown waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </form>
     </div>
   </div>
